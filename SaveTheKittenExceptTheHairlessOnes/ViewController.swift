9fe25e2cdc057232c8b965674f83dcf8288f2fa4
wotmn <aonurunver@gmail.com>
2018-02-11 18:47:53 +0200

    inital Commit

--- SaveTheKittenExceptTheHairlessOnes/ViewController.swift
+++ SaveTheKittenExceptTheHairlessOnes/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("work with bit bucket")
     }
 
     override func didReceiveMemoryWarning() {
